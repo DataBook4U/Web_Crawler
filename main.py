85ace0611db261e66ac5acfa9debc96ec49b70a3
DataBook4U (Sebastian) <opocensky.sebastian@outlook.de>
2023-03-09 19:14:37 +0100

    simple requests

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 
 
 #Import libraries:
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+
+r = requests.get("https://www.spacex.com/")
+print(r.status_code)
+print(r.headers)
+print(r.text)
